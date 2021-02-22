98fc0bdf6b3cf7f2dbb5a4f141784a8c9d546946
Arthur A. Selimov <am35a.piston@gmail.com>
2021-02-22 15:34:19 +0300

    fix #8

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
     // import successkid from 'images/successkid.jpg';
+    // export let fullscreen = false
 </script>
 
 <style>
@@ -10,6 +11,14 @@
     <title>TriaFly UI Kit - CSS/JS framework for TriaFly BI system</title>
 </svelte:head>
 
+<!-- <div>
+    <button
+        on:click="{() => fullscreen = !fullscreen}"
+        class="tf_btn tf_btn-secondary float-right"
+    >
+        <i class="fas {fullscreen ? 'fa-expand' : 'fa-compress'}"></i>
+    </button>
+</div> -->
 <div class="d-grid g-3">
     <div>
         <!-- <img class="d-inline-flex" src="images/logo-icon.svg" alt="TriaFly"> -->
