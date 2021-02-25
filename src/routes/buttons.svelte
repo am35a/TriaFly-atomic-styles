6f0ee5ddfb7def6a382b52d0f6021cdda798453e
Arthur Selimov <arthur.selimov@triafly.ru>
2021-02-25 17:26:29 +0300

    add description for buttons page

--- src/routes/buttons.svelte
+++ src/routes/buttons.svelte
@@ -1,22 +1,64 @@
+<script>
+    import { Highlight } from "svelte-highlight"
+    import { xml } from "svelte-highlight/languages"
+</script>
+
 <svelte:head>
 	<title>TriaFly UI Kit - buttons</title>
 </svelte:head>
 
 <div class="h1" role="heading" aria-level="1">Buttons</div>
+<p>
+    To create a default button, use the tf_btn class, but default state is not recommended to use without color classes for any buttons.
+</p>
 
-<div class="h2" role="heading" aria-level="2">default</div>
+<div class="h2" role="heading" aria-level="2">Default</div>
 <p>
     <button class="tf_btn">default</button>
 </p>
+The following classes defining button colors:
+<ul>
+    <li>
+        .tf_btn-primary
+    </li>
+    <li>
+        .tf_btn-secondary
+    </li>
+    <li>
+        .tf_btn-info
+    </li>
+    <li>
+        .tf_btn-success
+    </li>
+    <li>
+        .tf_btn-warning
+    </li>
+    <li>
+        .tf_btn-error
+    </li>
+</ul>
 
 <div class="h2" role="heading" aria-level="2">Primary</div>
-<p>
+<p>One of the main colors button is primary.</p>
+<button class="tf_btn tf_btn-primary">text button</button>
+<button class="tf_btn tf_btn-primary"><i class="fas fa-home"></i> icon button</button>
+<button class="tf_btn tf_btn-primary" disabled>disabled button</button>
+<Highlight class="d-inline" language="{xml}" code='
+    // primary color button
     <button class="tf_btn tf_btn-primary tf_btn-lg">text button</button>
-    <button class="tf_btn tf_btn-primary tf_btn-lg"><i class="fas fa-home"></i> icon button</button>
-    <button class="tf_btn tf_btn-primary tf_btn-lg" disabled>disabled button</button>
-</p>
+
+    // primary color button with icon
+    <button class="tf_btn tf_btn-primary">
+        <i class="fas fa-home"></i>
+        icon button
+    </button>
+
+    // primary color disabled button
+    <button class="tf_btn tf_btn-primary" disabled>disabled button</button>
+'/>
 
 <div class="h2" role="heading" aria-level="2">Secondary</div>
+<p>Next of the main colors button is secondary.</p>
 <p>
     <button class="tf_btn tf_btn-secondary">text button</button>
     <button class="tf_btn tf_btn-secondary"><i class="fas fa-home"></i> icon button</button>
@@ -24,7 +66,9 @@
 </p>
 
 <div class="h2" role="heading" aria-level="2">Addition color buttons</div>
-
+<p>
+    The following classes defining action button colors:
+</p>
 <div class="h3" role="heading" aria-level="3">Info</div>
 <p>
     <button class="tf_btn tf_btn-info">text button</button>
@@ -52,9 +96,20 @@
     <button class="tf_btn tf_btn-error" disabled>disabled button</button>
 </p>
 
-<div class="h2" role="heading" aria-level="2">sizes</div>
+<div class="h2" role="heading" aria-level="2">Sizes</div>
 <p>
-    <button class="tf_btn tf_btn-primary tf_btn-sm">text button</button>
-    <button class="tf_btn tf_btn-primary">text button</button>
-    <button class="tf_btn tf_btn-primary tf_btn-lg">text button</button>
+    For change size of buttons use tf_btn-sm and tf_btn-lg classes:
 </p>
+<button class="tf_btn tf_btn-primary tf_btn-sm">text button</button>
+<button class="tf_btn tf_btn-primary">text button</button>
+<button class="tf_btn tf_btn-primary tf_btn-lg">text button</button>
+<Highlight class="d-inline" language="{xml}" code='
+    // small size
+    <button class="tf_btn tf_btn-primary tf_btn-sm">text button</button>
+
+    // normal
+    <button class="tf_btn tf_btn-primary">text button</button>
+
+    // large size
+    <button class="tf_btn tf_btn-primary tf_btn-lg">text button</button>
+'/>
