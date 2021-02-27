bc6bd83255569019f6432437e3226ca1cf1e6ffa
Arthur A. Selimov <am35a.piston@gmail.com>
2021-02-27 14:22:10 +0300

    add icon buttons

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -35,14 +35,19 @@
     <div class="text-uppercase">
         {segment === undefined ? 'Home' : segment}
     </div>
-    <div class="d-flex align-items-center ml-auto h-100">
+    <div class="d-flex align-items-center g-1 ml-auto h-100">
         <button
-            class="tf_btn px-3 h-100"
+            class="tf_btn tf_btn-lg tf_btn-transparent tf_btn-icon"
             on:click={() => inverTheme = !inverTheme}
         >
             <i class="fas fa-fill-drip"></i>
         </button>
-        <a class="d-flex text-decoration-none px-2 h-100" href="https://github.com/am35a/TriaFly-atomic-styles" target="_blank"><i class="fab fa-github fa-2x m-auto"></i></a>
+        <button
+            class="tf_btn tf_btn-lg tf_btn-transparent tf_btn-icon mr-1"
+            on:click={() => window.location = "https://github.com/am35a/TriaFly-atomic-styles"}
+        >
+            <i class="fab fa-github fa-lg"></i>
+        </button>
     </div>
 </header>
 <aside class="tf_layout-aside {$state.isAsideExpand ? 'tf_expand' : ''}">
--- src/routes/buttons.svelte
+++ src/routes/buttons.svelte
@@ -8,42 +8,101 @@
     To create a default button, use the tf_btn class, but default state is not recommended to use without color classes for any buttons.
 </p>
 
-<div class="h2" role="heading" aria-level="2">Default</div>
-<p>
-    <button class="tf_btn">text button</button>
+<div id="default" class="h2" role="heading" aria-level="2">Default</div>
+<div class="d-flex g-1 flex-wrap mb-3">
+    <button class="tf_btn">default button</button>
     <button class="tf_btn"><i class="fas fa-home"></i> icon button</button>
     <button class="tf_btn" disabled>disabled button</button>
-</p>
-The following classes defining button colors:
-<ul>
-    <li>
-        .tf_btn-primary
-    </li>
-    <li>
-        .tf_btn-secondary
-    </li>
-    <li>
-        .tf_btn-info
-    </li>
-    <li>
-        .tf_btn-success
-    </li>
-    <li>
-        .tf_btn-warning
-    </li>
-    <li>
-        .tf_btn-error
-    </li>
-</ul>
+</div>
+<Highlight language="{xml}" code='
+    // inverse color button
+    <button class="tf_btn">default button</button>
 
-<div class="h2" role="heading" aria-level="2">Primary</div>
+    // inverse color button with icon
+    <button class="tf_btn">
+        <i class="fas fa-home"></i>
+        icon button
+    </button>
+
+    // inverse color disabled button
+    <button class="tf_btn" disabled>disabled button</button>
+'/>
+
+<div id="inverse" class="h2" role="heading" aria-level="2">Inverse</div>
+<p>Inverted color buttons are transparent like buttons by default.</p>
+<div class="d-flex g-1 flex-wrap mb-3">
+    <button class="tf_btn tf_btn-inverse">inverse button</button>
+    <button class="tf_btn tf_btn-inverse"><i class="fas fa-home"></i> icon button</button>
+    <button class="tf_btn tf_btn-inverse" disabled>disabled button</button>
+</div>
+<Highlight language="{xml}" code='
+    // inverse color button
+    <button class="tf_btn tf_btn-inverse">inverse button</button>
+
+    // inverse color button with icon
+    <button class="tf_btn tf_btn-inverse">
+        <i class="fas fa-home"></i>
+        icon button
+    </button>
+
+    // inverse color disabled button
+    <button class="tf_btn tf_btn-inverse" disabled>disabled button</button>
+'/>
+
+<div id="transparent" class="h2" role="heading" aria-level="2">Transparent</div>
+<div class="d-flex g-1 flex-wrap mb-3">
+    <button class="tf_btn tf_btn-transparent">transparent button</button>
+    <button class="tf_btn tf_btn-transparent"><i class="fas fa-home"></i> icon button</button>
+    <button class="tf_btn tf_btn-transparent" disabled>disabled button</button>
+</div>
+<Highlight language="{xml}" code='
+    // transparent button
+    <button class="tf_btn tf_btn-transparent">transparent button</button>
+
+    // transparent button with icon
+    <button class="tf_btn tf_btn-transparent">
+        <i class="fas fa-home"></i>
+        icon button
+    </button>
+
+    // transparent disabled button
+    <button class="tf_btn tf_btn-transparent" disabled>disabled button</button>
+'/>
+
+<div>
+    The following classes defining button colors:
+    <ul>
+        <li class="text-primary">
+            .tf_btn-primary
+        </li>
+        <li class="text-secondary">
+            .tf_btn-secondary
+        </li>
+        <li class="text-info">
+            .tf_btn-info
+        </li>
+        <li class="text-success">
+            .tf_btn-success
+        </li>
+        <li class="text-warning">
+            .tf_btn-warning
+        </li>
+        <li class="text-error">
+            .tf_btn-error
+        </li>
+    </ul>
+</div>
+
+<div id="primary" class="h2" role="heading" aria-level="2">Primary</div>
 <p>One of the main colors button is primary.</p>
-<button class="tf_btn tf_btn-primary">text button</button>
-<button class="tf_btn tf_btn-primary"><i class="fas fa-home"></i> icon button</button>
-<button class="tf_btn tf_btn-primary" disabled>disabled button</button>
-<Highlight class="d-inline" language="{xml}" code='
+<div class="d-flex g-1 flex-wrap mb-3">
+    <button class="tf_btn tf_btn-primary">primary button</button>
+    <button class="tf_btn tf_btn-primary"><i class="fas fa-home"></i> icon button</button>
+    <button class="tf_btn tf_btn-primary" disabled>disabled button</button>
+</div>
+<Highlight language="{xml}" code='
     // primary color button
-    <button class="tf_btn tf_btn-primary tf_btn-lg">text button</button>
+    <button class="tf_btn tf_btn-primary">primary button</button>
 
     // primary color button with icon
     <button class="tf_btn tf_btn-primary">
@@ -55,59 +114,82 @@ The following classes defining button colors:
     <button class="tf_btn tf_btn-primary" disabled>disabled button</button>
 '/>
 
-<div class="h2" role="heading" aria-level="2">Secondary</div>
+<div id="secondary" class="h2" role="heading" aria-level="2">Secondary</div>
 <p>Next of the main colors button is secondary.</p>
-<p>
-    <button class="tf_btn tf_btn-secondary">text button</button>
+<div class="d-flex g-1 flex-wrap mb-3">
+    <button class="tf_btn tf_btn-secondary">secondary button</button>
     <button class="tf_btn tf_btn-secondary"><i class="fas fa-home"></i> icon button</button>
     <button class="tf_btn tf_btn-secondary" disabled>disabled button</button>
-</p>
+</div>
 
 <div class="h2" role="heading" aria-level="2">Addition color buttons</div>
-<p>
-    The following classes defining action button colors:
-</p>
-<div class="h3" role="heading" aria-level="3">Info</div>
-<p>
-    <button class="tf_btn tf_btn-info">text button</button>
+<p>The following classes defining action button colors:</p>
+
+<div id="info" class="h3" role="heading" aria-level="3">Info</div>
+<div class="d-flex g-1 flex-wrap mb-3">
+    <button class="tf_btn tf_btn-info">info button</button>
     <button class="tf_btn tf_btn-info"><i class="fas fa-home"></i> icon button</button>
     <button class="tf_btn tf_btn-info" disabled>disabled button</button>
-</p>
+</div>
 
-<div class="h3" role="heading" aria-level="3">Success</div>
-<p>
-    <button class="tf_btn tf_btn-success">text button</button>
+<div id="success" class="h3" role="heading" aria-level="3">Success</div>
+<div class="d-flex g-1 flex-wrap mb-3">
+    <button class="tf_btn tf_btn-success">success button</button>
     <button class="tf_btn tf_btn-success"><i class="fas fa-home"></i> icon button</button>
     <button class="tf_btn tf_btn-success" disabled>disabled button</button>
-</p>
+</div>
 
-<div class="h3" role="heading" aria-level="3">Warning</div>
-<p>
-    <button class="tf_btn tf_btn-warning">text button</button>
+<div id="warning" class="h3" role="heading" aria-level="3">Warning</div>
+<div class="d-flex g-1 flex-wrap mb-3">
+    <button class="tf_btn tf_btn-warning">warning button</button>
     <button class="tf_btn tf_btn-warning"><i class="fas fa-home"></i> icon button</button>
     <button class="tf_btn tf_btn-warning" disabled>disabled button</button>
-</p>
-<div class="h3" role="heading" aria-level="3">Error</div>
-<p>
-    <button class="tf_btn tf_btn-error">text button</button>
+</div>
+
+<div id="error" class="h3" role="heading" aria-level="3">Error</div>
+<div class="d-flex g-1 flex-wrap mb-3">
+    <button class="tf_btn tf_btn-error">error button</button>
     <button class="tf_btn tf_btn-error"><i class="fas fa-home"></i> icon button</button>
     <button class="tf_btn tf_btn-error" disabled>disabled button</button>
-</p>
+</div>
 
-<div class="h2" role="heading" aria-level="2">Sizes</div>
-<p>
-    For change size of buttons use tf_btn-sm and tf_btn-lg classes:
-</p>
-<button class="tf_btn tf_btn-primary tf_btn-sm">text button</button>
-<button class="tf_btn tf_btn-primary">text button</button>
-<button class="tf_btn tf_btn-primary tf_btn-lg">text button</button>
-<Highlight class="d-inline" language="{xml}" code='
-    // small size
-    <button class="tf_btn tf_btn-primary tf_btn-sm">text button</button>
+<div id="sizes" class="h2" role="heading" aria-level="2">Sizes</div>
+<p>For change size of buttons use tf_btn-sm and tf_btn-lg classes:</p>
+<div class="d-flex g-1 flex-wrap align-items-center mb-3">
+    <button class="tf_btn tf_btn-primary tf_btn-sm">small button</button>
+    <button class="tf_btn tf_btn-primary">default button</button>
+    <button class="tf_btn tf_btn-primary tf_btn-lg">large button</button>
+</div>
+<Highlight language="{xml}" code='
+    // small size button
+    <button class="tf_btn tf_btn-primary tf_btn-sm">small button</button>
 
-    // normal
-    <button class="tf_btn tf_btn-primary">text button</button>
+    // normal size button
+    <button class="tf_btn tf_btn-primary">default button</button>
 
-    // large size
-    <button class="tf_btn tf_btn-primary tf_btn-lg">text button</button>
+    // large size button
+    <button class="tf_btn tf_btn-primary tf_btn-lg">large button</button>
+'/>
+
+<div id="icon" class="h2" role="heading" aria-level="2">Icon</div>
+<div class="d-flex g-1 flex-wrap align-items-center mb-3">
+    <button class="tf_btn tf_btn-primary tf_btn-icon tf_btn-sm"><i class="fas fa-info"></i></button>
+    <button class="tf_btn tf_btn-primary tf_btn-icon"><i class="fas fa-infinity"></i></button>
+    <button class="tf_btn tf_btn-primary tf_btn-icon tf_btn-lg"><i class="fas fa-brain"></i></button>
+</div>
+<Highlight language="{xml}" code='
+    // icon small size button
+    <button class="tf_btn tf_btn-primary tf_btn-icon tf_btn-sm">
+        <i class="fas fa-info"></i>
+    </button>
+
+    // icon normal size button
+    <button class="tf_btn tf_btn-primary tf_btn-icon">
+        <i class="fas fa-infinity"></i>
+    </button>
+
+    // icon large size button
+    <button class="tf_btn tf_btn-primary tf_btn-icon tf_btn-lg">
+        <i class="fas fa-brain"></i>
+    </button>
 '/>
