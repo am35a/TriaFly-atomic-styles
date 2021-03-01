338d44196fe3c38dc5ea16719145356fbe73abec
Arthur A. Selimov <am35a.piston@gmail.com>
2021-03-01 10:31:36 +0300

    added background-color classes

--- src/components/icons/Blocks.svelte
+++ src/components/icons/Blocks.svelte
@@ -1,4 +1,4 @@
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
     <rect fill="none" height="24" width="24"/>
-    <path d="M3,3v18h18V3H3z M14,17H7v-2h7V17z M17,13H7v-2h10V13z M17,9H7V7h10V9z"/>
+    <path d="M3,3v18h18V3H3z M14,18H7v-2h7V18z M17,14H7v-2h10V14z M17,7H7V5h10V7z M20,10H4V9h16V10z"/>
 </svg>
