3e54e810b379be35c1cc03d6a20763a9d30faf82
Arthur A. Selimov <am35a.piston@gmail.com>
2021-03-06 23:08:02 +0300

    preventAssignment = true

--- rollup.config.js
+++ rollup.config.js
@@ -24,6 +24,7 @@ export default {
 		output: config.client.output(),
 		plugins: [
 			replace({
+				preventAssignment: true,
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
@@ -74,6 +75,7 @@ export default {
 		output: config.server.output(),
 		plugins: [
 			replace({
+				preventAssignment: true,
 				'process.browser': false,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
@@ -107,6 +109,7 @@ export default {
 		plugins: [
 			resolve(),
 			replace({
+				preventAssignment: true,
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
