43ee46fb08dcbb4dbcffcd58059f7cc79358faef
Arthur A. Selimov <am35a.piston@gmail.com>
2021-03-09 13:06:06 +0300

    add simple grid

--- src/routes/ex-layout-2x2.svelte
+++ src/routes/ex-layout-2x2.svelte
@@ -9,7 +9,7 @@
 	<title>TriaFly UI Kit - layout: 2x2 grid with blocks</title>
 </svelte:head>
 
-<div class="d-grid g-3" style="grid-template-columns: 1fr 1fr;grid-template-rows: 1fr 1fr;min-height: 100%;">
+<div class="d-grid cols-2 rows-2 g-3 h-100"> <!-- style="grid-template-columns: 1fr 1fr;grid-template-rows: 1fr 1fr;min-height: 100%;" -->
     <div class="tf_block tf_block-secondary">
         <div class="tf_block-header">
             Secondary block title
@@ -23,10 +23,10 @@
     </div>
     <div class="tf_block tf_block-secondary">
         <div class="tf_block-header">
-            Secondary block title
+            Secondary block title <button class="tf_btn tf_btn-sm ml-auto"><i class="fas fa-home"></i> icon button</button>
         </div>
         <div class="tf_block-body">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores!
         </div>
         <div class="tf_block-footer">
             Footer
@@ -37,7 +37,7 @@
             Secondary block title
         </div>
         <div class="tf_block-body">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum, ex excepturi enim quis explicabo, tempora natus mollitia at eos odit consequatur rerum deserunt officia voluptatibus earum dignissimos voluptatum in maiores!
         </div>
         <div class="tf_block-footer">
             Footer
