68eca6c8e2db6a90438f8a5672b729b3c55ade21
Arthur A. Selimov <am35a.piston@gmail.com>
2021-03-18 13:17:56 +0300

    beautify utilities

--- src/routes/layout.svelte
+++ src/routes/layout.svelte
@@ -161,7 +161,23 @@
                 class="rounded-2 rounded-top-0 mt-0"
                 language='{xml}'
                 code='
-    ...
+    <div class="tf_layout-modal-loader">
+        <div class="m-auto bg-positive px-5 py-3 rounded d-flex">
+            <svg viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
+                <g fill="none" fill-rule="evenodd">
+                    <g transform="translate(1 1)" stroke-width="2">
+                        <circle stroke-opacity=".25" cx="18" cy="18" r="18"/>
+                        <path d="M36 18c0-9.94-8.06-18-18-18">
+                            <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"/>
+                        </path>
+                    </g>
+                </g>
+            </svg>
+            <div class="my-auto ml-3">
+                Loading data...
+            </div>
+        </div>
+    </div>
                 '
             />
         </div>
--- src/routes/utilities.svelte
+++ src/routes/utilities.svelte
@@ -14,6 +14,8 @@
                 <a href="utilities#bg-color">Background colors</a>
                 <a href="utilities#border">Border</a>
                 <a href="utilities#radius">Radius</a>
+                <a href="utilities#clearfix">Clearfix</a>
+                <a href="utilities#display">Display</a>
             </div>
         </div>
     </div>
@@ -177,9 +179,8 @@
         </div>
     </div>
 
-    <div id="Radius" class="overflow-auto">
+    <div id="radius" class="overflow-auto">
         <div class="h2" role="heading" aria-level="2">Radius</div>
-        <!-- <div class="h3" role="heading" aria-level="3">Radius</div> -->
         <p></p>
         <div>
             <div class="bg-background rounded-2 rounded-bottom-0 p-3 text-center">
@@ -194,6 +195,12 @@
                         <div class="bg-positive border p-5 rounded-circle"></div>
                         <div class="bg-positive border p-5 rounded-pill" style="width: 16rem;"></div>
                     </div>
+                    <div class="d-flex flex-wrap g-3">
+                        <div class="bg-positive border p-5 rounded-circle rounded-left-0"></div>
+                        <div class="bg-positive border p-5 rounded-circle rounded-top-0"></div>
+                        <div class="bg-positive border p-5 rounded-circle rounded-right-0"></div>
+                        <div class="bg-positive border p-5 rounded-circle rounded-bottom-0"></div>
+                    </div>
                 </div>
             </div>
             <Highlight
@@ -201,16 +208,83 @@
                 language='{xml}'
                 code='
     <div class="rounded-0"></div>
-    <div class="rounded-sm"></div>
+    <div class="rounded-sm"></div> /* or rounded-1 */
     <div class="rounded"></div>
-    <div class="rounded-lg"></div>
+    <div class="rounded-lg"></div> /* or rounded-3 */
 
     <div class="rounded-circle"></div>
     <div class="rounded-pill"></div>
+
+    <div class="rounded-circle rounded-left-0"></div>
+    <div class="rounded-circle rounded-top-0"></div>
+    <div class="rounded-circle rounded-right-0"></div>
+    <div class="rounded-circle rounded-bottom-0"></div>
                 '
             />
         </div>
 
+    </div>
+
+    <div id="clearfix" class="overflow-auto">
+        <div class="h2" role="heading" aria-level="2">Clearfix</div>
+        <p>
+            To expand and cover floated content by a container just add <b>.clearfix</b> class to container.
+        </p>
+        <div>
+            <div class="bg-background rounded-2 rounded-bottom-0 p-3">
+                <div class="d-grid g-3">
+                    <div class="clearfix">
+                        <div class="bg-negative">
+                            <div class="bg-positive p-3 float-left">Floated left block</div>
+                            <div class="bg-positive p-3 float-right">Floated left block</div>
+                        </div>
+                    </div>
+    
+                    <div class="bg-negative clearfix">
+                        <div class="bg-positive p-3 float-left">Floated left block</div>
+                        <div class="bg-positive p-3 float-right">Floated left block</div>
+                    </div>
+                </div>
+            </div>
+            <Highlight
+                class="rounded-2 rounded-top-0 mt-0"
+                language='{xml}'
+                code='
+    <div class="clearfix">
+        <div class="float-left">Floated left block</div>
+        <div class="float-right">Floated left block</div>
+    </div>
+            '
+            />
+        </div>
+    </div>
+
+    <div id="display" class="overflow-auto">
+        <div class="h2" role="heading" aria-level="2">Display</div>
+        <p>
+            For change the display value of tags use follow display classes:
+        </p>
+        <ul>
+            <li>.d-inline</li>
+            <li>.d-block</li>
+            <li>.d-inline-block</li>
+            <li>.d-grid</li>
+            <li>.d-inline-grid</li>
+            <li>.d-flex</li>
+            <li>.d-inline-flex</li>
+            <li class="text-warning">.d-table</li>
+            <li class="text-warning">.d-table-row</li>
+            <li class="text-warning">.d-table-cell</li>
+            <li>.d-none</li>
+            <li>.d-unset</li>
+        </ul>
+        <p>For hiding an element use <b>.d-none</b> class.</p>
+        <p class="rounded-2 bg-warning p-3">
+            <i class="fas fa-exclamation-circle"></i> Pay attention: <b>.d-inline</b> can ignore upsizing and margin for focus on baseline.
+        </p>
+        <p class="rounded-2 bg-warning p-3">
+            <i class="fas fa-exclamation-circle"></i> <b>.table</b>, <b>.table-row</b> and <b>.table-cell</b> values it's not better idea to use. Choose another way to goal.
+        </p>
     </div>
 
 </div>
