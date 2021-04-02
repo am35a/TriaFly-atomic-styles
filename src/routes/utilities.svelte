2ffaaca0e464677a1e18f456d71e2919cdaede55
Arthur A. Selimov <am35a.piston@gmail.com>
2021-04-02 13:52:47 +0300

    Update Display ito utilities docs

--- src/routes/utilities.svelte
+++ src/routes/utilities.svelte
@@ -4,7 +4,8 @@
     import BgColor    from "../routes/utilities/BgColor.svelte"
     import Border     from "../routes/utilities/Border.svelte"
     import Radius     from "../routes/utilities/Radius.svelte"
-    import Clearfix     from "../routes/utilities/Clearfix.svelte"
+    import Clearfix   from "../routes/utilities/Clearfix.svelte"
+    import Display    from "../routes/utilities/Display.svelte"
 </script>
 
 <PageLayout>
@@ -22,8 +23,8 @@
             <a href="utilities#add-border">- Add Border</a>
             <a href="utilities#cut-border">- Cut Border</a>
         <a href="utilities#radius">Radius</a>
-        <!-- <a href="utilities#clearfix">Clearfix</a>
-        <a href="utilities#display">Display</a> -->
+        <a href="utilities#clearfix">Clearfix</a>
+        <a href="utilities#display">Display</a>
     </slot>
     <div id="utilities" class="h1" role="heading" aria-level="1">Utilities</div>
     <Alignment/>
@@ -31,39 +32,5 @@
     <Border/>
     <Radius/>
     <Clearfix/>
+    <Display/>
 </PageLayout>
-
-
-<div class="d-grid g-3 d-none">
-
-    <div id="display" class="overflow-auto">
-        <div class="h2" role="heading" aria-level="2">Display</div>
-        <p>
-            For change the display value of tags use follow display classes:
-        </p>
-        <ul>
-            <li>.d-inline</li>
-            <li>.d-block</li>
-            <li>.d-inline-block</li>
-            <li>.d-grid</li>
-            <li>.d-inline-grid</li>
-            <li>.d-flex</li>
-            <li>.d-inline-flex</li>
-            <li class="text-warning">.d-table</li>
-            <li class="text-warning">.d-table-row</li>
-            <li class="text-warning">.d-table-cell</li>
-            <li>.d-none</li>
-            <li>.d-unset</li>
-        </ul>
-        <p>For hiding an element use <b>.d-none</b> class.</p>
-        <p class="rounded-2 bg-warning p-3">
-            <i class="fas fa-exclamation-circle"></i> Pay attention: <b>.d-inline</b> can ignore upsizing and margin for focus on baseline.
-        </p>
-        <p class="rounded-2 bg-warning p-3">
-            <i class="fas fa-exclamation-circle"></i> <b>.table</b>, <b>.table-row</b> and <b>.table-cell</b> values it's not better idea to use. Choose another way to goal.
-        </p>
-    </div>
-
-</div>
-
-
---
+++ src/routes/utilities/Display.svelte
@@ -0,0 +1,27 @@
+<div id="display">
+    <div class="h2" role="heading" aria-level="2">Display</div>
+    <p>
+        For change the display value of tags use follow display classes:
+    </p>
+    <ul>
+        <li>.d-inline</li>
+        <li>.d-block</li>
+        <li>.d-inline-block</li>
+        <li>.d-grid</li>
+        <li>.d-inline-grid</li>
+        <li>.d-flex</li>
+        <li>.d-inline-flex</li>
+        <li class="text-warning">.d-table</li>
+        <li class="text-warning">.d-table-row</li>
+        <li class="text-warning">.d-table-cell</li>
+        <li>.d-none</li>
+        <li>.d-unset</li>
+    </ul>
+    <p>For hiding an element use <b>.d-none</b> class.</p>
+    <p class="rounded-2 bg-warning p-3">
+        <i class="fas fa-exclamation-circle"></i> Pay attention: <b>.d-inline</b> can ignore upsizing and margin for focus on baseline.
+    </p>
+    <p class="rounded-2 bg-warning p-3">
+        <i class="fas fa-exclamation-circle"></i> <b>.table</b>, <b>.table-row</b> and <b>.table-cell</b> values it's not better idea to use. Choose another way to goal.
+    </p>
+</div>
