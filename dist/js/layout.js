38bf3f7d6c23cbec92303a7708f07ab49add0d32
Arthur A. Selimov <am35a.piston@gmail.com>
2021-11-30 07:12:08 +0300

    added aside body fixes

--- dist/js/layout.js
+++ dist/js/layout.js
@@ -5,3 +5,12 @@ $( ".tf_layout-aside-expander" ).on( "click", function() {
 $( ".tf_aside-fader" ).on( "click", function() {
     $( ".tf_layout-aside" ).removeClass( "tf_expand" );
 });
+
+
+$('.tf_layout-aside[class*=tf_fixed]').on('click','.tf_aside-logo', function(){
+    if($('.tf_layout-aside').hasClass('tf_fixed'))
+        $('.tf_layout-aside').removeClass('tf_fixed').addClass('tf_fixed-open');
+    else 
+        if($('.tf_layout-aside').hasClass('tf_fixed-open'))
+            $('.tf_layout-aside').addClass('tf_fixed').removeClass('tf_fixed-open');
+});
--- dist/js/ui-drops.js
+++ dist/js/ui-drops.js
@@ -1,10 +1,10 @@
 let isDropBtnActive = false
 window.onmousedown = function(event) {
-    if (event.target.matches('.tf_drop-focus .tf_btn-drop, .tf_drop-focus tf_btn-drop *'))
+    if (event.target.matches('.tf_drop-focus .tf_btn-drop, .tf_drop-focus .tf_btn-drop *'))
         event.target == document.activeElement ? isDropBtnActive = true : isDropBtnActive = false
 }
 window.onmouseup = function(event) {
-    if (event.target.matches('.tf_drop-focus .tf_btn-drop, .tf_drop-focus tf_btn-drop *'))
+    if (event.target.matches('.tf_drop-focus .tf_btn-drop, .tf_drop-focus .tf_btn-drop *'))
         if (isDropBtnActive) {
             event.target.blur()
             isDropBtnActive = false
