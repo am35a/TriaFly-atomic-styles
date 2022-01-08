25328bd5235f27518c0734b1fc51ac3a7ef83888
Arthur A. Selimov <am35a.piston@gmail.com>
2022-01-08 14:42:01 +0300

    fix  !important

--- dist/js/layout.js
+++ dist/js/layout.js
@@ -2,7 +2,7 @@ $( ".tf_layout-aside-expander" ).on( "click", function() {
     if( !$( "tf_layout" ).hasClass( "desktop" ) )
         $( ".tf_layout-aside " ).addClass( "tf_expand" );
 });
-$( ".tf_aside-fader" ).on( "click", function() {
+$( ".tf_aside-fader, .mobile .tf_navbar-item, .tablet .tf_navbar-item" ).on( "click", function() {
     $( ".tf_layout-aside" ).removeClass( "tf_expand" );
 });
 
